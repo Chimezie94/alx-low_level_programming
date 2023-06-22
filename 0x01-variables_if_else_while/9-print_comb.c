76ba0b256881c89c3fe23b8501358737ead6295d
Chimezie94 <chimezieokoye90@yahoo.com>
2023-06-22 14:22:42 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Eachtime 0 (Success)
+ */
+int main(void){
+int b;
+for
+(b = 0; b < 10; b++)
+{
+putchar(b + '0');
+if (b < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
