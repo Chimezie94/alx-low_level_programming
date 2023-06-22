4eb6aaced92ad5760d9048d4ec5bf51dffdf1942
Chimezie94 <chimezieokoye90@yahoo.com>
2023-06-22 08:15:12 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Eachtime 0 (Success)
+ */
+int main(void)
+{
+int i;
+i = '0';
+while
+(i <= 10)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
