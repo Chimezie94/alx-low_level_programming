f31a10cb8c06478f023f0935bd411670cb885ed8
Chimezie94 <chimezieokoye90@yahoo.com>
2023-06-21 23:55:39 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Eachtime 0 (Success)
+ */
+int main(void)
+{
+char e;
+char f;
+e = 'a'
+f = 'A'
+while (e <= 'z')
+{
+putchar (e);
+e++;
+}
+while (f <= 'Z')
+{
+putchar (f);
+f++;
+}
+putchar ('\n');
+return (0);
+}
+
+
