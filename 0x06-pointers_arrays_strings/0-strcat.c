367398dbaa9d48e64e882c7022de7b42dd1cb9ff
Chimezie94 <chimezieokoye90@yahoo.com>
2023-06-28 23:29:11 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
 /**
  * main - concatenate two strings
+ * return: eachtime 0 (Success)
+ */
+char *_strcat(char *dest, char *src)
+{
+int i;
+int j;
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
+}
