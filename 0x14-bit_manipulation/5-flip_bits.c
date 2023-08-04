5b553db6aba8763ee92e8051f44ec82f6ee23009
Chimezie94 <chimezieokoye90@yahoo.com>
2023-08-04 02:40:56 -0700

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * flip_bits - this funct counts the number of bits to change
+ * to get from one number to another
+ * @n1: is the first number
+ * @m2: is the second number
+ *
+ * Return: number of bits to change
+ */
+unsigned int flip_bits(unsigned long int n1, unsigned long int m1)
+{
+int i, count = 0;
+unsigned long int current;
+unsigned long int exclusive = n1 ^ m1;
+for (i = 63; i >= 0; i--)
+{
+current = exclusive >> i;
+if (current & 1)
+count++;
+}
+return (count);
+}
